c2c1fa1448603b6f759b74479ea70730169e9041
Hunter David Lecavalier <hunter@blazegeek.com>
2024-04-04 16:51:24 -0400

    Handles renaming geekcash -> blaze in shell files

--- contrib/auto_gdb/blaze_dbg.sh
+++ contrib/auto_gdb/blaze_dbg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # use testnet settings,  if you need mainnet,  use ~/.blaze/blazed.pid file instead
-geekcash_pid=$(<~/.blaze/testnet3/blazed.pid)
-sudo gdb -batch -ex "source debug.gdb" blazed ${geekcash_pid}
+blaze_pid=$(<~/.blaze/testnet3/blazed.pid)
+sudo gdb -batch -ex "source debug.gdb" blazed ${blaze_pid}
