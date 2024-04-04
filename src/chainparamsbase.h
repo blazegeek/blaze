b37735918ec98166679e1de493225a2bf1dfdc94
Hunter David Lecavalier <hunter@blazegeek.com>
2024-04-04 15:56:52 -0400

    Handles renaming GeekCash -> Blaze in C++ files and headers

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -10,7 +10,7 @@
 
 /**
  * CBaseChainParams defines the base parameters (shared between blaze-cli and blazed)
- * of a given instance of the GeekCash system.
+ * of a given instance of the Blaze system.
  */
 class CBaseChainParams
 {
--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -30,7 +30,7 @@ class QUrl;
 class QWidget;
 QT_END_NAMESPACE
 
-/** Utility functions used by the GeekCash Qt UI.
+/** Utility functions used by the Blaze Qt UI.
  */
 namespace GUIUtil
 {
