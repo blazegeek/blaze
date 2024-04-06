741eb615d190404c2fa4bf29fb1a42a69edaa131
Hunter David Lecavalier <hunter@blazegeek.com>
2024-04-06 14:38:17 -0400

    Updates github repo links

--- src/init.cpp
+++ src/init.cpp
@@ -650,10 +650,10 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/blazegeek-com/blaze>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/blazegeek/blaze>";
     const std::string URL_WEBSITE = "<https://www.blazegeek.com>";
 
-    return CopyrightHolders(_("Copyright (C)"), 2024, COPYRIGHT_YEAR) + "\n" +
+    return CopyrightHolders(_("Copyright (c)"), 2024, COPYRIGHT_YEAR) + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
